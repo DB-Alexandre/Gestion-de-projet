4169d206ec7f34cda63a814fe6f7a606aaa19a15
DB-Alexandre <88594845+DB-Alexandre@users.noreply.github.com>
2025-01-29 10:33:09 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import { logger } from '../src/lib/logger.ts';
+import { logger } from './logger.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
